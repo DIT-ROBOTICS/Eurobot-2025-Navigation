a11ae7d38d5580dbf2caa0e3ea852f75af857768
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-11-25 18:23:54 +0800

    try to stop : add find squard by time

--- path_executor/src/dock10Executor.cpp
+++ path_executor/src/dock10Executor.cpp
@@ -204,6 +204,7 @@ void Dock10Executor::timerCB(const ros::TimerEvent& e) {
             }
             case MODE::ESCAPE: {
                 escape();
+                findSquardCost(pose_[0],pose_[1]);
                 if(!needEscape()) mode_ = MODE::IDLE;
                 break;
             }
